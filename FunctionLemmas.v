0903f1aa2f827728005cacc84643d750b0be48d4
nbrader <nbrader+github@gmail.com>
2024-10-12 17:55:04 +0100

    Moved code into library from https://github.com/nbrader/proofs-taylor-polynomials/

--- FunctionLemmas.v
+++ FunctionLemmas.v
@@ -20,3 +20,9 @@ Proof.
   rewrite <- Heq.
   reflexivity.
 Qed.
+
+Fixpoint iter {A : Type} (f : A -> A) (n : nat) (x : A) : A :=
+  match n with
+  | 0 => x
+  | S n' => f (iter f n' x)
+  end.
--- ListFunctions.v
+++ ListFunctions.v
@@ -52,6 +52,22 @@ Proof.
     apply comH.
 Qed.
 
+(* Allows us to expand a left fold as if it were a right fold, provided the operation allows a form of commutativity. *)
+Lemma fold_left_cons_comm_2 : forall [A B : Type] (f : B -> A -> B) (x : A) (xs : list A) (i : B),
+  (forall (i : B), commutative (fun x y => f (f i x) y)) -> fold_left f (x :: xs) i = f (fold_left f xs i) x.
+Proof.
+  intros A B f x xs i comH.
+  simpl. (* This reduces `fold_left f (x :: xs) i` to `fold_left f xs (f i x)` *)
+  revert i. (* We prepare to use induction on `xs` *)
+  induction xs as [|x' xs' IH]; intros i.
+  - simpl. (* Base case: `fold_left f [] (f i x)` simplifies to `f i x` *)
+    reflexivity.
+  - simpl in *. (* Inductive case: unfold the definition for one more element *)
+    rewrite <- (IH (f i x')). (* Use the induction hypothesis *)
+    f_equal.
+    apply comH.
+Qed.
+
 (* Lemma fold_left_comm_cons_app : forall [A B : Type] (f : A -> A -> A) (x : A) (xs ys : list A) (i : A),
   commutative f -> fold_left f ((x :: xs) ++ ys) i = fold_left f (xs ++ (x :: ys)) i.
 Proof.
@@ -106,4 +122,40 @@ Proof.
   intros.
   simpl.
   reflexivity.
+Qed.
+
+Theorem fold_left_right_equiv : 
+  forall (A : Type) (f : A -> A -> A) (z : A) (l : list A),
+    (forall x y z, f x (f y z) = f (f x y) z) -> (* Associativity of f *)
+    (forall x y, f x y = f y x) -> (* Associativity of f *)
+    fold_left f l z = fold_right f z l.
+Proof.
+  intros A f z l H_assoc H_comm.
+  revert z. (* We generalize z to allow induction *)
+  induction l as [| x xs IH]; intros z.
+  - (* Base case: empty list *)
+    reflexivity.
+  - (* Inductive step: non-empty list *)
+    simpl. (* Simplify both fold_left and fold_right *)
+    rewrite <- IH. (* Apply the inductive hypothesis *)
+    clear IH. (* No longer need the inductive hypothesis *)
+    (* Now use the associativity of f to rearrange *)
+    induction xs as [| y ys IHys]; simpl.
+    + (* Case: xs = [] *)
+      apply H_comm.
+    + (* Case: xs = y :: ys *)
+      rewrite (H_comm x (fold_left f ys (f z y))).
+      rewrite <- (fold_left_cons_comm).
+      * simpl.
+        rewrite <- (H_assoc z y x).
+        rewrite (H_comm y x).
+        rewrite (H_assoc z x y).
+        reflexivity.
+      * intros.
+        unfold ssrfun.commutative.
+        intros.
+        rewrite <- H_assoc.
+        rewrite (H_comm x0 y0).
+        rewrite H_assoc.
+        reflexivity.
 Qed.
