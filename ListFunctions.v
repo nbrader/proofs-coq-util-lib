99827796c27e351cf4fb9af8b7a01718a9292269
nbrader <nbrader+github@gmail.com>
2024-10-12 20:43:55 +0100

    Whoops! I didn't mean to commit the duplication of this theorem

--- ListFunctions.v
+++ ListFunctions.v
@@ -52,22 +52,6 @@ Proof.
     apply comH.
 Qed.
 
-(* Allows us to expand a left fold as if it were a right fold, provided the operation allows a form of commutativity. *)
-Lemma fold_left_cons_comm_2 : forall [A B : Type] (f : B -> A -> B) (x : A) (xs : list A) (i : B),
-  (forall (i : B), commutative (fun x y => f (f i x) y)) -> fold_left f (x :: xs) i = f (fold_left f xs i) x.
-Proof.
-  intros A B f x xs i comH.
-  simpl. (* This reduces `fold_left f (x :: xs) i` to `fold_left f xs (f i x)` *)
-  revert i. (* We prepare to use induction on `xs` *)
-  induction xs as [|x' xs' IH]; intros i.
-  - simpl. (* Base case: `fold_left f [] (f i x)` simplifies to `f i x` *)
-    reflexivity.
-  - simpl in *. (* Inductive case: unfold the definition for one more element *)
-    rewrite <- (IH (f i x')). (* Use the induction hypothesis *)
-    f_equal.
-    apply comH.
-Qed.
-
 (* Lemma fold_left_comm_cons_app : forall [A B : Type] (f : A -> A -> A) (x : A) (xs ys : list A) (i : A),
   commutative f -> fold_left f ((x :: xs) ++ ys) i = fold_left f (xs ++ (x :: ys)) i.
 Proof.
